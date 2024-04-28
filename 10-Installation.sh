655235639b1197fd29f3f656ecfea2f798b2f794
Aamani <m.aamani401@gmail.com>
2024-04-28 11:31:26 +0530

    if loop

--- 10-Installation.sh
+++ 10-Installation.sh
@@ -10,5 +10,11 @@ else
     echo "You are a superuser"
 fi
 
-dnf install mysql -f
+dnf install mysql -y
+if [ $? -ne 0]
+then
+    echo "installation of mysql is not success"
+    exit 1
+fi
+
 echo "is script proceeding?"
