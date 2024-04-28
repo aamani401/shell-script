fca7d86b62c2bfb09db688af0ce9addf15d46220
Aamani <m.aamani401@gmail.com>
2024-04-28 07:16:20 +0530

    operatos

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-Movies={"RRR","Bahaubali","Adithya369"}
+Movies={"RRR" "Bahaubali" "Adithya369"}
 
-echo $Movies[0]
-#echo ${Movies[1]}
-#echo ${Movies[@]}
+echo ${Movies[0]}
+echo ${Movies[1]}
+echo ${Movies[@]}
