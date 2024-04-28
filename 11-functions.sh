1494ad1f85b0bfcfe80211239cd9ca03415fd611
Aamani <m.aamani401@gmail.com>
2024-04-28 12:26:41 +0530

    if loop

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+USERID=$(id -u)
+echo "$USERID"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is failure"
+    else
+        echo "$2 is success"
+
+    fi
+}
+if [ $USERID -ne 0 ]
+then 
+    echo "run script with super user access"
+    exit 1
+else 
+    echo "You are a superuser"
+fi
+
+dnf install mysql -y
+VALIDATE $? "Installing mysql"
