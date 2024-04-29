bd9dfff7ed2d9812cc1b6c13ecbb7e050657eb65
Aamani <m.aamani401@gmail.com>
2024-04-29 12:14:27 +0530

    test[F

--- ExpensesProject/test.sh
+++ ExpensesProject/test.sh
@@ -5,5 +5,8 @@ DATETIME=$(date +%F-%H-%M-%S)
 FILENAME=$( echo $0 | cut -d '.' -f1 )
 LOGFILENAME=/tmp/$FILENAME-$DATETIME.LOG
 
-
+echo "$DATETIME"
+echo "$FILENAME"
+echo "$LOGFILENAME"
 dnf install mysql-server -y &>>LOGFILENAME
+echo "$? output"
