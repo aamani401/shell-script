bc1949e015c018732317c8b38c5584b77a219347
aamani401 <166613197+aamani401@users.noreply.github.com>
2024-05-04 16:30:53 +0530

    Delete 03-variables.sh

--- 03-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#declare variables
-person1=sekhar #nospace between = and value
-person2=bram
-#referring variables
-echo "$person1:Hi $person2" # "$" is considered it as variable
-echo "$person2: Hi $person1 how are you:"
-echo "$person1 : Am good $person2 how is work"
