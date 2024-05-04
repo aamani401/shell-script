5d8ad239cc766efc6dc4248a66637b93b016cdc3
aamani401 <166613197+aamani401@users.noreply.github.com>
2024-05-04 16:31:19 +0530

    Delete 05-variables.sh

--- 05-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Please enter username"
-read USERNAME #username will be stored under USERNAME variable
-echo "Please enter password"
-read -s PASSWORD
-
-echo "Provided username: $USERNAME and password is $PASSWORD"
